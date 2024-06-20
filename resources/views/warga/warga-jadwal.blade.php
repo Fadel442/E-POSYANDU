ec9599548f8b74effcfbb8b604a36e66db634411
Fadel442 <fadelmuhammad4402@gmail.com>
2024-06-20 08:29:46 +0700

    sweet alert

--- resources/views/warga/warga-jadwal.blade.php
+++ resources/views/warga/warga-jadwal.blade.php
@@ -11,33 +11,12 @@
         @foreach ($posts as $post)
         <div class="bg-[#1A4D2E] m-5 p-2 rounded-lg px-5 mb-10">
             <div>
-                <p class="text-white font-bold text-3xl">Jadwal Posyandu</p>
-                <p class="text-white font-semibold text-m flex flex-wrap border-b border-white ">{{$post->bulan}} Tahun {{$post->tahun}}</p>
-                <p class="text-white font-light text-sm mt-1 mb-4 ">DESA SUKOMANUNGGAL. KEC ABONSARI</p>
-                <p class="text-white">{{$post->hari}}, {{$post->tanggal}}</p>
-                <p class="text-white font-bold text-3xl  ">{{$post->lokasi}}</p>
-                {{-- <div class="flex align-content-end">
-                    <a href="{{route ('detail-jadwal')}}" class="text-white font-semibold text-sm mt-2">Lihat Selengkapnya</a>
-                </div> --}}
+                <p class="text-white font-bold text-3xl">{{$post->tempat}}</p>
+                <p class="text-white font-semibold text-m flex flex-wrap border-b border-white ">{{$post->tanggal}}</p>
+                <p class="text-white font-semibold text-sm mt-1 mb-2 ">RT {{$post->rt}}, RW {{$post->rw}}</p>
             </div>
         </div>
         @endforeach
-        {{-- <div class="bg-[#1A4D2E] m-5 p-2 rounded-lg px-5 mb-10">
-            <div>
-                <p class="text-white font-bold text-3xl">Jadwal Posyandu</p>
-                <p class="text-white font-semibold text-m flex flex-wrap border-b border-white ">Januari Tahun 2024</p>
-                <p class="text-white font-light text-sm mt-1">DESA SUKOMANUNGGAL. KEC ABONSARI</p>
-                <div class="flex align-content-end">
-                    <a href="{{route('detail-jadwal')}}" class="text-white font-semibold text-sm mt-2">Lihat Selengkapnya</a>
-                </div>
-            </div>
-        </div> --}}
-        
-        {{-- <div class="w-full h-full flex flex-col justify-start bg-[#1A4D2E] m-5">
-            <p class="text-white font-semibold text-xl">Jadwal Posyandu</p>
-            <p class="text-white font-semibold text-xl">Januari Tahun 2024</p>
-        </div> --}}
-       
     </div>
 </div>
     
